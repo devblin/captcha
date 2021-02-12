cfc5529ca3539c41e57c60f32b639963b14b1cd6
devblin <deepanshu.dhruw@outlook.com>
2021-02-12 22:41:07 +0530

    update composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '215f726fbe9d6b3890bbc05e76a30ff306388df0',
+    'name' => 'captcha/web',
+  ),
+  'versions' => 
+  array (
+    'captcha/web' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '215f726fbe9d6b3890bbc05e76a30ff306388df0',
+    ),
+  ),
+);
